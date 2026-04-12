f4076dfa1c80c3f4bb5954308cfba44825273dd5
Marshall Ku <marshall@kakao.com>
2026-04-12 23:15:06 +0900

    Remove dead codes

--- turm-core/src/error.rs
+++ turm-core/src/error.rs
@@ -2,17 +2,11 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum TurmError {
-    #[error("PTY error: {0}")]
-    Pty(String),
-
     #[error("IO error: {0}")]
     Io(#[from] std::io::Error),
 
     #[error("Config error: {0}")]
     Config(String),
-
-    #[error("Session not found: {0}")]
-    SessionNotFound(String),
 
     #[error("Protocol error: {0}")]
     Protocol(String),
--- turm-core/src/lib.rs
+++ turm-core/src/lib.rs
@@ -2,6 +2,4 @@ pub mod config;
 pub mod error;
 pub mod plugin;
 pub mod protocol;
-pub mod pty;
-pub mod state;
 pub mod theme;
